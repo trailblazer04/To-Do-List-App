c5a8c29bc8ca0e5fe91121f9c1c95fa298bc3795
trailblazer04 <leofrancis6988@gmail.com>
2025-08-05 20:23:52 +0530

    Added function click to checked and for span to remove the list

--- TDL/script.js
+++ TDL/script.js
@@ -14,5 +14,13 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = "";
-
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
